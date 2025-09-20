b1604a1eb155560d34d9bed5cb7e6d2bdbda4021
AlishaMeier <alisha.meyer@gmail.com>
2025-09-20 22:21:06 +0500

    half hw completed

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# qaguru_hm12
+# qaguru_hm11
+Lesson  11. Jenkins. Creating our first task. Stanislav Vasenkov 1. Practice – Creating a task (Job), linking it to a repository in Github, running tests remotely  2. Theory –  Jenkins basics – CI/CD basics
---
+++ conftest.py
@@ -0,0 +1,54 @@
+import pytest
+from selene import Browser, Config
+from selene.support.shared import config
+from selenium import webdriver
+from dotenv import load_dotenv
+import os
+from selenium.webdriver.chrome.options import Options
+from utils import attach
+from selene.support.shared import browser
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+    global selenoid_login, selenoid_pass, selenoid_url
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
+    print("SELENOID_LOGIN =", selenoid_login)
+    print("SELENOID_PASS =", selenoid_pass)
+    print("SELENOID_URL =", selenoid_url)
+
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser():
+    options = Options()
+    caps = {
+        "browserName": "chrome",
+        #"browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(caps)
+
+    options.capabilities.update(caps)
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",   #f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub" #"https://user1:1234@selenoid.autotests.cloud/wd/hub"
+        options=options)
+
+    browser.config.driver = driver
+
+    yield browser
+
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    driver.quit()
+
+
